4264bdb71300758e8dacbdb75d929744856e02dd
Yanick Gubler <ygubler@hsr.ch>
2016-10-16 17:31:03 +0200

    notesStore DB implementet module.exports = {add : publicAddNote, get : publicGet, all : publicAll}; missing methods to change properties or delete notes

--- services/notesStore.js
+++ services/notesStore.js
@@ -3,3 +3,39 @@
  */
 var Datastore = require('nedb');
 var db = new Datastore({ filename: './data/notes.db', autoload: true});
+
+function Note(noteTitle, noteDescription, noteImportance, noteDueDate, noteDone)
+{
+    this.noteTitle = noteTitle;
+    this.noteDescription = noteDescription;
+    this.noteImportance = noteImportance;
+    this.noteDueDate = noteDueDate;
+    this.noteDone = noteDone;
+}
+
+function publicAddNote(noteTitle, noteDescription, noteImportance, noteDueDate, noteDone, callback)
+{
+    var note = new Note(noteTitle, noteDescription, noteImportance, noteDueDate, noteDone);
+    db.insert(note, function(err, newDoc){
+        if(callback){
+            callback(err, newDoc);
+        }
+    });
+}
+
+function publicGet(id, callback)
+{
+    db.findOne({ _id: id }, function (err, doc) {
+        callback( err, doc);
+    });
+}
+
+function publicAll()
+{
+    db.find({}, function (err, docs) {
+        callback( err, docs);
+    });
+}
+
+
+module.exports = {add : publicAddNote, get : publicGet, all : publicAll};
