25582c5e2455a699eaaad2b79191e3ccc32766e0
Yanick Gubler <ygubler@hsr.ch>
2016-10-16 17:40:09 +0200

    initial notesController module.exports.createNote

--- controller/notesController.js
+++ controller/notesController.js
@@ -1,3 +1,18 @@
 /**
  * Created by Yanick on 16.10.2016.
  */
+var store = require("../services/notesStore");
+
+module.exports.createNote = function(req, res)
+{
+    var order = store.add(req.body.noteTitle, req.body.noteDescription, req.body.noteImportance, req.body.noteDueDate, req.body.noteDone, function (err, note) {
+        res.format({
+            'text/html': function(){
+                res.render("succeeded", order);
+            },
+            'application/json': function(){
+                res.json(order);
+            }
+        });
+    })
+}
