4dff91ec4394f062411decf2faf6fc0b7f10e140
Yanick Gubler <ygubler@hsr.ch>
2016-10-21 18:01:28 +0200

    Fix von Buttonsclass zu buttons in noteController da keine anzeuge möglich war anzeige von finished feld per finishdate gelöst

--- controller/notesController.js
+++ controller/notesController.js
@@ -4,11 +4,11 @@ create = function (req, res) {
     store.add(req.body.title, req.body.description, req.body.importance, req.body.dueDate, req.body.done, function (err, note) {
         res.redirect("/");
     });
-}
+};
 
 getAll = function (req, res) {
     var filter = req.query.show;
-    var baseparams =
+
     var buttons = {
         filter: {
             class: "",
@@ -39,11 +39,11 @@ getAll = function (req, res) {
     // Fall: Sort by importnace desc dann den Filter ein und aus schalten sollte die sortierung nicht veraendern.
     if(filter == "all"){
         buttons.filter.class = "active";
-        buttons.filter.action = "/?"+req.query
-        query.filter = ""
-        filter = {}
+        buttons.filter.action = "/?"+req.query;
+        query.filter = "";
+        filter = {};
     } else {
-        buttonclass.filter = "";
+        buttons.filter = "";
         filter = { done: "false" }
     }
 
@@ -56,35 +56,35 @@ getAll = function (req, res) {
     switch(orderBy){
         case "finish":
             orderBy = { finishDate: 1 * direction };
-            buttonclass.finish = "active" + direction?" asc":" desc";
+            buttons.finish = "active" + direction?" asc":" desc";
             break;
         case "create": orderBy = { createDate: 1 * direction };
-            buttonclass.create = "active" + direction?" asc":" desc";
+            buttons.create = "active" + direction?" asc":" desc";
             break;
         case "importance": orderBy = { importance: -1 * direction };
-            buttonclass.importance = "active" + direction?" asc":" desc";
+            buttons.importance = "active" + direction?" asc":" desc";
             break;
         default:
             orderBy = { createDate: -1 }
-            buttonclass.create = "active" + direction?" asc":" desc";
+            buttons.create = "active" + direction?" asc":" desc";
     }
 
 
     store.all(filter, orderBy, function (err, data) {
-        res.render('index', {buttonclass: buttonclass, notes: data});
+        res.render('index', {buttons: buttons, notes: data});
     });
-}
+};
 
 get = function (req, res) {
     store.get(req.params.id, function (err, data) {
         res.render('note', {note: data});
     });
-}
+};
 
 update = function(req, res){
     store.update(req.params.id, req.body.title, req.body.description, req.body.importance, req.body.dueDate, req.body.done, function (err, numReplaced){
         res.redirect("/");
     });
-}
+};
 
 module.exports = {create: create, get: get, getAll: getAll, update: update};
